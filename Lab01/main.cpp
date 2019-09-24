c2ceae278e0fd60d8d2a1367e0ee7463b1bbfe22
U-VLAB\user-lab01 <user-lab01@win10-lab1-16.vlab.bmstu.ru>
2019-09-24 11:24:53 +0300

    created number class

--- Lab01/main.cpp
+++ Lab01/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Number
+{
+public:
+    Number(string str)
+    {
+        input = str;
+        cout << str;
+    }
+    string input;
+};
+
+void input()
+{
+    cout << "Enter first number: ";
+    string input;
+    cin >> input;
+    Number n1(input);
+    return;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    input();
     return 0;
 }
