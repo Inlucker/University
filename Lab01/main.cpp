004f2d0f79c16946e9bbfbba2e0e6c696dbaa15d
U-VLAB\user-lab01 <user-lab01@win10-lab1-16.vlab.bmstu.ru>
2019-09-24 11:51:24 +0300

    started making input func

--- Lab01/main.cpp
+++ Lab01/main.cpp
@@ -9,9 +9,40 @@ public:
     Number(string str)
     {
         input = str;
-        cout << str;
+        int flag = 0;
+        cout << str.length() << '\n';
+        for (int i = 0; i <= str.length(); i++)
+        {
+            int j = 0;
+            int tochka = 0;
+            if (flag == 0)
+            {
+                if (str[i] == '+' || str[i] == '-')
+                {
+                    znak = str[i];
+                    flag++;
+                }
+            }
+            else if (flag == 1)
+            {
+                if (str[i] >= '0' && str[i] <= '9' || str[i] == '.')
+                {
+                    if (str[i] == '.')
+                        tochka++;
+                    if (tochka <= 1)
+                        mantisa[j] = str[i];
+                    else
+                        input_error = true;
+                }
+            }
+        }
     }
     string input;
+    bool input_error;
+    char znak;
+    char mantisa[30];
+    char zn_por;
+    char porydok[5];
 };
 
 void input()
