2753abfb1b5092c3846ba40c553bf1f4b9df288e
Arseny Pronin <arseny707@mail.ru>
2020-09-24 02:52:56 +0300

    same operation problem solved for lab of 2019

--- Lab01-2019/main.cpp
+++ Lab01-2019/main.cpp
@@ -11,312 +11,312 @@ using namespace std;
 class Number
 {
 public:
-	Number(string str)
-	{
-		input = str;
-		znak = '+';
-		zn_por = '+';
+    Number(string str)
+    {
+        input = str;
+        znak = '+';
+        zn_por = '+';
         int tmp_i = 0;
-		if (str[0] == '+' || str[0] == '-')
+        if (str[0] == '+' || str[0] == '-')
         {
             znak = str[0];
             tmp_i++;
         }
-		for (int i = 0; i < 30; i++)
-			mantisa[i] = '0';
-		mant = "";
-		poryd = "";
-		int do_tochki = 0;
-		int posle_tochki = 0;
-		int flag = 0;
+        for (int i = 0; i < 30; i++)
+            mantisa[i] = '0';
+        mant = "";
+        poryd = "";
+        int do_tochki = 0;
+        int posle_tochki = 0;
+        int flag = 0;
         for (int i = 0 + tmp_i; i < str.length(); i++)
-		{
-			int j = 0;
+        {
+            int j = 0;
 
-			if (flag == 0)
-			{
-				if (str[i] >= '1' && str[i] <= '9')
-					flag = 1;
-				else if (str[i] == '.')
-					flag = 2;
+            if (flag == 0)
+            {
+                if (str[i] >= '1' && str[i] <= '9')
+                    flag = 1;
+                else if (str[i] == '.')
+                    flag = 2;
                 else if (str[i] != '0')
                 {
                     input_error = true;
                     break;
                 }
-			}
+            }
 
-			switch (flag)
-			{
-			case 1:
-				if (str[i] >= '0' && str[i] <= '9')
-				{
-					mant += str[i];
-					do_tochki++;
-				}
-				else if (str[i] == '.')
-					flag = 3;
-				else if (str[i] == 'e' || str[i] == 'E')
-					flag = 4;
+            switch (flag)
+            {
+            case 1:
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    mant += str[i];
+                    do_tochki++;
+                }
+                else if (str[i] == '.')
+                    flag = 3;
+                else if (str[i] == 'e' || str[i] == 'E')
+                    flag = 4;
                 else
                     input_error = true;
-				break;
-			case 2:
-				if (str[i] == '0')
-					posle_tochki++;
+                break;
+            case 2:
+                if (str[i] == '0')
+                    posle_tochki++;
                 else if (str[i] >= '1' && str[i] <= '9')
-				{
-					mant += str[i];
-					flag = 3;
-				}
+                {
+                    mant += str[i];
+                    flag = 3;
+                }
                 else if (str[i] != '.')
                     input_error = true;
-				break;
-			case 3:
-				if (str[i] >= '0' && str[i] <= '9')
-				{
-					mant += str[i];
-				}
+                break;
+            case 3:
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    mant += str[i];
+                }
                 else if (str[i] == 'e' || str[i] == 'E')
                 {
                     flag = 4;
                 }
-				else
-                    input_error = true;
-				break;
-			case 4:
-				if (str[i] == '+' || str[i] == '-')
-					zn_por = str[i];
-				else if (str[i] >= '0' && str[i] <= '9')
-					poryd += str[i];
                 else
                     input_error = true;
-				flag = 5;
-				break;
-			case 5:
-				if (str[i] >= '0' && str[i] <= '9')
-					poryd += str[i];
+                break;
+            case 4:
+                if (str[i] == '+' || str[i] == '-')
+                    zn_por = str[i];
+                else if (str[i] >= '0' && str[i] <= '9')
+                    poryd += str[i];
                 else
                     input_error = true;
-				break;
-			}
+                flag = 5;
+                break;
+            case 5:
+                if (str[i] >= '0' && str[i] <= '9')
+                    poryd += str[i];
+                else
+                    input_error = true;
+                break;
+            }
             if (do_tochki > 30)
                 input_error = true;
-		}
+        }
 
-		mantisa_len = mant.length();
-		int j = 29;
-		for (int i = mantisa_len - 1; i >= 0; i--)
-		{
-			mantisa[j] = mant[i];
-			j--;
-		}
-		if (mant == "")
-			mant = "0";
+        mantisa_len = mant.length();
+        int j = 29;
+        for (int i = mantisa_len - 1; i >= 0; i--)
+        {
+            mantisa[j] = mant[i];
+            j--;
+        }
+        if (mant == "")
+            mant = "0";
 
-		if (flag == 1 || flag == 3)
-		{
+        if (flag == 1 || flag == 3)
+        {
             if (do_tochki - posle_tochki < 0) //|| flag == 1)
-				zn_por = '-';
-			else
-				zn_por = '+';
+                zn_por = '-';
+            else
+                zn_por = '+';
 
-			int tmp_por = abs(do_tochki - posle_tochki);
-			por = do_tochki - posle_tochki;
-			while (tmp_por > 0)
-			{
-				poryd += 48+(tmp_por % 10);
-				tmp_por /= 10;
-			}
-			if (poryd.length() == 2)
-				swap(poryd[0], poryd[1]);
-			/*for (int i = 0; i < poryd.length() / 2; i++)
-			{
-				swap(poryd[i], poryd[poryd.length() - i - 1]);
-			}*/
-		}
-		if (flag == 5)
-		{
-			int tmp_por = 0;
-			j = 1;
-			for (int i = poryd.length() - 1; i >= 0; i--)
-			{
-				tmp_por += (poryd[i] - 48) * j;
-				j *= 10;
-			}
-			if (zn_por == '+')
-				tmp_por += do_tochki;
-			else
-				tmp_por = do_tochki - tmp_por;
-			por = tmp_por;
-			//cout << tmp_por << endl;
-			poryd = "";
-			if (tmp_por < 0)
-				zn_por = '-';
-			else
-				zn_por = '+';
-			tmp_por = abs(tmp_por);
+            int tmp_por = abs(do_tochki - posle_tochki);
+            por = do_tochki - posle_tochki;
+            while (tmp_por > 0)
+            {
+                poryd += 48+(tmp_por % 10);
+                tmp_por /= 10;
+            }
+            if (poryd.length() == 2)
+                swap(poryd[0], poryd[1]);
+            /*for (int i = 0; i < poryd.length() / 2; i++)
+            {
+                swap(poryd[i], poryd[poryd.length() - i - 1]);
+            }*/
+        }
+        if (flag == 5)
+        {
+            int tmp_por = 0;
+            j = 1;
+            for (int i = poryd.length() - 1; i >= 0; i--)
+            {
+                tmp_por += (poryd[i] - 48) * j;
+                j *= 10;
+            }
+            if (zn_por == '+')
+                tmp_por += do_tochki;
+            else
+                tmp_por = do_tochki - tmp_por;
+            por = tmp_por;
+            //cout << tmp_por << endl;
+            poryd = "";
+            if (tmp_por < 0)
+                zn_por = '-';
+            else
+                zn_por = '+';
+            tmp_por = abs(tmp_por);
 
-			while (tmp_por > 0)
-			{
-				poryd += 48+(tmp_por % 10);
-				tmp_por /= 10;
-			}
-			for (int i = 0; i < poryd.length() / 2; i++)
-			{
-				swap(poryd[i], poryd[poryd.length() - i - 1]);
-			}
-		}
+            while (tmp_por > 0)
+            {
+                poryd += 48+(tmp_por % 10);
+                tmp_por /= 10;
+            }
+            for (int i = 0; i < poryd.length() / 2; i++)
+            {
+                swap(poryd[i], poryd[poryd.length() - i - 1]);
+            }
+        }
 
         if  (poryd.length() > 5)
             input_error = true;
 
-		if (poryd == "")
-			poryd = "0";
+        if (poryd == "")
+            poryd = "0";
 
-		/*j = 4;
-		for (int i = 0; i < poryd.length(); i++)
-		{
-			porydok[j] = poryd[i];
-			j--;
-		}*/
-		//cout << por << endl;
+        /*j = 4;
+        for (int i = 0; i < poryd.length(); i++)
+        {
+            porydok[j] = poryd[i];
+            j--;
+        }*/
+        //cout << por << endl;
 
-		//cout << mantisa_len << endl;
-		/*for (int i = 0; i < 30; i++)
-			cout << mantisa[i];
-		cout << endl;*/
-		//cout << znak << "0." << mant << "E" << zn_por << poryd << '.' << endl;
-		//print();
+        //cout << mantisa_len << endl;
+        /*for (int i = 0; i < 30; i++)
+            cout << mantisa[i];
+        cout << endl;*/
+        //cout << znak << "0." << mant << "E" << zn_por << poryd << '.' << endl;
+        //print();
     }
-	void print()
-	{
+    void print()
+    {
         //if (!input_error)
         cout << znak << "0." << mant << "E" << zn_por << poryd << '.' << endl;
         /*else
             cout << "This number is not correct" << endl;*/
-	}
-	string input;
+    }
+    string input;
     bool input_error = false;
-	char znak;
-	char mantisa[30];
-	string mant;
-	char zn_por;
-	//char porydok[5];
-	string poryd;
-	int mantisa_len;
-	int por;
+    char znak;
+    char mantisa[30];
+    string mant;
+    char zn_por;
+    //char porydok[5];
+    string poryd;
+    int mantisa_len;
+    int por;
 };
 
 //Функция сложения двух чисел по разрядам
-void summa (int rez[30], int n1[30], int n2[30])
+int summa (int rez[30], int n1[30], int n2[30])
 {
-	int tmp = 0;
+    int tmp = 0;
 
-	for (int i = 29; i >= 0; i--)
-		rez[i] = 0;
+    for (int i = 29; i >= 0; i--)
+        rez[i] = 0;
 
-	for (int i = 29; i >= 0; i--)
-	{
-		rez[i] = n1[i] + n2[i] + tmp;
-		tmp = 0;
+    for (int i = 29; i >= 0; i--)
+    {
+        rez[i] = n1[i] + n2[i] + tmp;
+        tmp = 0;
 
-		if (rez[i] >= 10)
-		{
-			rez[i] -=10;
-			tmp++;
-		}
-	}
+        if (rez[i] >= 10)
+        {
+            rez[i] -=10;
+            tmp++;
+        }
+    }
 
-	return;
+    return tmp;
 }
 
 //Функция разночти двух чисел по разрядам
 void raznost (int rez[30], int n1[30], int n2[30])
 {
-	int tmp = 0;
+    int tmp = 0;
 
-	for (int i = 29; i >= 0; i--)
-		rez[i] = 0;
+    for (int i = 29; i >= 0; i--)
+        rez[i] = 0;
 
-	for (int i = 29; i >= 0; i--)
-	{
-		rez[i] = n1[i] - n2[i] - tmp;
-		tmp = 0;
+    for (int i = 29; i >= 0; i--)
+    {
+        rez[i] = n1[i] - n2[i] - tmp;
+        tmp = 0;
 
-		if (rez[i] < 0)
-		{
-			rez[i] +=10;
-			tmp++;
-		}
-	}
+        if (rez[i] < 0)
+        {
+            rez[i] +=10;
+            tmp++;
+        }
+    }
 
-	return;
+    return;
 }
 
 //Функция сравнения двух чисел хранящихся в массивах по разрядам
 int sravnenie (int n1[30], int n2[30])
 {
-	int rez = 0;
-	for (int i = 0; i < 30; i++)
-	{
-		if (n1[i] < n2[i])
-		{
-			rez = -1;
-			break;
-		}
-		else if (n1[i] > n2[i])
-		{
-			rez = 1;
-			break;
-		}
-	}
-	return rez;
+    int rez = 0;
+    for (int i = 0; i < 30; i++)
+    {
+        if (n1[i] < n2[i])
+        {
+            rez = -1;
+            break;
+        }
+        else if (n1[i] > n2[i])
+        {
+            rez = 1;
+            break;
+        }
+    }
+    return rez;
 }
 
 //Функция получения определённой части числа хранящегося в массиве по разрядам
 void get (int rez[30], int input[30], int n)
 {
-	int j = 0;
-	while (input[j] == 0)
-	{
-		j++;
-	}
-	for (int i = 0; i < 30; i++)
-	{
+    int j = 0;
+    while (input[j] == 0)
+    {
+        j++;
+    }
+    for (int i = 0; i < 30; i++)
+    {
         if (i < 30 - n || j >= 30)
-			rez[i] = 0;
-		else
-		{
-			rez[i] = input[j];
-			j++;
-		}
-	}
+            rez[i] = 0;
+        else
+        {
+            rez[i] = input[j];
+            j++;
+        }
+    }
 }
 
 //Функция добавления в конец числа новой цифры
 void plus_razryad (int rez[30], int n)
 {
-	for (int i = 0; i < 29; i++)
-	{
-		rez[i] = rez[i + 1];
-	}
-	rez[29] = n;
+    for (int i = 0; i < 29; i++)
+    {
+        rez[i] = rez[i + 1];
+    }
+    rez[29] = n;
 }
 
 //Функция присваивания для чисел хранящихся в массивах по разрядам
 void prirovnyat (int rez[30], int input[30])
 {
-	for (int i = 0; i < 30; i++)
-	{
-		rez[i] = input[i];
-	}
+    for (int i = 0; i < 30; i++)
+    {
+        rez[i] = input[i];
+    }
 }
 
 //Функция деления вещественных чисел
 Number delenie(Number n1, Number n2)
 {
-	string rez = "";
+    string rez = "";
 
     if ((n1.znak == '-') != (n2.znak == '-'))
         rez += "-";
@@ -324,8 +324,8 @@ Number delenie(Number n1, Number n2)
         rez += "+";
 
     bool is_zero1 = true;
-	int delimoe_mantisa[30];
-	for (int i = 0; i < 30; i++)
+    int delimoe_mantisa[30];
+    for (int i = 0; i < 30; i++)
     {
         delimoe_mantisa[i] = n1.mantisa[i] - 48;
         if (n1.mantisa[i] != '0')
@@ -333,8 +333,8 @@ Number delenie(Number n1, Number n2)
     }
 
     bool is_zero2 = true;
-	int delitel_mantisa[30];
-	for (int i = 0; i < 30; i++)
+    int delitel_mantisa[30];
+    for (int i = 0; i < 30; i++)
     {
         delitel_mantisa[i] = n2.mantisa[i] - 48;
         if (n2.mantisa[i] != '0')
@@ -355,16 +355,16 @@ Number delenie(Number n1, Number n2)
 
     int j = 0;
     if (sravnenie(delimoe_mantisa, delitel_mantisa) == ravno)
-	{
+    {
         rez += "100000000000000000000000000000";
         j = 30;
-	}
+    }
 
-	int tmp1_mantisa[30];
-	get(tmp1_mantisa, delimoe_mantisa, n2.mantisa_len);
-	int tmp2_mantisa[30];
-	prirovnyat(tmp2_mantisa, delitel_mantisa);
-	int tmp3_mantisa[30];
+    int tmp1_mantisa[30];
+    get(tmp1_mantisa, delimoe_mantisa, n2.mantisa_len);
+    int tmp2_mantisa[30];
+    prirovnyat(tmp2_mantisa, delitel_mantisa);
+    int tmp3_mantisa[30];
 
     int raznica_por = n1.mantisa_len - n2.mantisa_len;
 
@@ -388,25 +388,30 @@ Number delenie(Number n1, Number n2)
         raznica_por--;
     }
 
-	int k = 0;
-	int stepen = 0;
+    int k = 0;
+    int stepen = 0;
     while (j < 30)
     {
-		prirovnyat(tmp2_mantisa, delitel_mantisa);
+        prirovnyat(tmp2_mantisa, delitel_mantisa);
         while (sravnenie(tmp1_mantisa, tmp2_mantisa) != menshe)
-		{
-			summa(tmp3_mantisa, tmp2_mantisa, delitel_mantisa);
-			prirovnyat(tmp2_mantisa, tmp3_mantisa);
-			k++;
+        {
+            if (summa(tmp3_mantisa, tmp2_mantisa, delitel_mantisa) == 1)
+            {
+               prirovnyat(tmp3_mantisa, tmp2_mantisa);
+               k++;
+               break;
+            }
+            prirovnyat(tmp2_mantisa, tmp3_mantisa);
+            k++;
             /*cout << j << ":" << endl;
-			for (int i = 0; i < 30; i++)
-				{
-					cout << tmp1_mantisa[i] << " ";
-				}
-				cout << endl;
-			for (int i = 0; i < 30; i++)
-					{
-						cout << tmp2_mantisa[i] << " ";
+            for (int i = 0; i < 30; i++)
+                {
+                    cout << tmp1_mantisa[i] << " ";
+                }
+                cout << endl;
+            for (int i = 0; i < 30; i++)
+                    {
+                        cout << tmp2_mantisa[i] << " ";
                     }
                         cout << endl;*/
         }
@@ -416,8 +421,11 @@ Number delenie(Number n1, Number n2)
         //cout << rez << endl;
         k = 0;
 
-        raznost(tmp3_mantisa, tmp2_mantisa, delitel_mantisa);
-        prirovnyat(tmp2_mantisa, tmp3_mantisa);
+        if (sravnenie(tmp2_mantisa, tmp1_mantisa) == bolshe)
+        {
+            raznost(tmp3_mantisa, tmp2_mantisa, delitel_mantisa);
+            prirovnyat(tmp2_mantisa, tmp3_mantisa);
+        }
 
         raznost(tmp3_mantisa, tmp1_mantisa, tmp2_mantisa);
         prirovnyat(tmp1_mantisa, tmp3_mantisa);
@@ -468,8 +476,8 @@ Number delenie(Number n1, Number n2)
             }
         }
     }
-    if (j > 29)
-        j = 29;
+    /*if (j > 29)
+        j = 29;*/
     if (rez.find(".") == -1)
     {
         rez += "E";
@@ -499,8 +507,8 @@ Number delenie(Number n1, Number n2)
     }
 
     //cout << rez << endl;
-	Number chastnoe(rez);
-	return chastnoe;
+    Number chastnoe(rez);
+    return chastnoe;
 }
 
 int main()
@@ -530,5 +538,5 @@ int main()
     {
         cout << "Input Error" << endl;
     }*/
-	return 0;
+    return 0;
 }
